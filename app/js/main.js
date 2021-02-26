47d9cb3845fca1711fbae3ddf4a70884bb17ea38
SeniorDmitriy <dimaponomarenko443@gmail.com>
2021-02-26 21:50:06 +0200

    added design section, add video block

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,23 @@
-$(function(){
+$(function () {
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    },
+    animation: {
+      duration: 350
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true,
     fade: true
   });
 
-  
-  var mixer = mixitup('.product__items', {
-    animation: {
-        duration: 300
-    }
-})
 });
